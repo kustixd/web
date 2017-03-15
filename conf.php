0f27883d5c66a596301152b3b87993ca47567834
KustixD <karl-gustav.kraak@khk.ee>
2017-03-15 15:34:42 +0200

    Created application configuration. First version.

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KustixD
+ * Date: 15.03.2017
+ * Time: 15:25
+ */
+//degineerime vajalikud konstandid
+define('CLASSES_DIR', 'classes/'); //classes kataloogi nime konstant
+define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi nime konstant
+?>
--- index.php
+++ index.php
@@ -5,5 +5,8 @@
  * Date: 15.03.2017
  * Time: 13:08
  */
+//votame konfiguratsiooni kasutusele
+require_once 'conf.php';
+//pealehe sisu
 echo '<h1>Veebiprogrammeerimise esileht </h1>';
 ?>
