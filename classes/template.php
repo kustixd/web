3a776d1580c2b8af0cb455c95a2fa50543b1e57d
KustixD <karl-gustav.kraak@khk.ee>
2017-03-22 12:49:49 +0200

    Checking weather file exist or not.

--- classes/template.php
+++ classes/template.php
@@ -13,6 +13,26 @@ class template
     var $content = false; // html malli faili sisu
     var $vars = array(); // html vaade sisu - reaalsed väärtused
     // klassi tegevused - meetodid - funktsioonid
+
+    // html faili lugemine
+    function loadFile(){
+        $f = $this->file; //lokaalne asendus
+        // kontrollime mallide kausta olemasolu
+        if(!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.' ei ole leitud</ br>';
+            exit;
+        }
+        // kui fail on olemas ja lugemiseks sobiv
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            // loeme faili sisu
+            $this->readFile($f);
+        }
+        // kui sisu polnud võimalik lugeda
+        if($this->content === false){
+            echo 'Ei suutnud lugeda faili '.$this->file.'<br />';
+        }
+    }// load file
+
     // loeme sisu html failist
     function readFile($f){
         $this->content = file_get_contents($f);
