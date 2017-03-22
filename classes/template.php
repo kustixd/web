c15c08732af688b8f392b7a3df589607bf3f945a
KustixD <karl-gustav.kraak@khk.ee>
2017-03-22 12:21:50 +0200

    First version of the template.php file.

---
+++ classes/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: KustixD
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{//klassi algus
+    //template klassi omadused - muutujad
+    var $file = ''; // html malli faili nimi
+    var $content = false; // html malli faili sisu
+    var $vars = array(); // html vaade sisu - reaalsed väärtused
+
+}//klassi lõpp
+?>
