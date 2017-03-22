47dae163bb11063e2545765e2f941d56c1533da4
KustixD <karl-gustav.kraak@khk.ee>
2017-03-22 14:38:15 +0200

    Added main page template values

--- index.php
+++ index.php
@@ -11,6 +11,12 @@ require_once 'conf.php';
 echo '<h1>Veebiprogrammeerimise esileht </h1>';
 // valmistame peatemplate objekti.
 $main_tmpl = new template('main');
+// valmistame paarid malli element => väärtus
+$main_tmpl->set('user', 'Kasutajanimi');
+$main_tmpl->set('title', 'Pealeht');
+$main_tmpl->set('lang_bar', 'Keeleriba');
+$main_tmpl->set('menu', 'Lehe peamenüü');
+$main_tmpl->set('content', 'Lehe sisu');
 // kontrollin antud objekti sisu
 echo '<pre>';
 print_r($main_tmpl);
