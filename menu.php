ddd93d355333abf0950c4a22f0a2d7be667f4bf2
KustixD <karl-gustav.kraak@khk.ee>
2017-03-22 15:30:32 +0200

    Checking objects existents and its content

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KustixD
+ * Date: 22.03.2017
+ * Time: 15:26
+ */
+// loome menüü template objektid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+// kontrollime objekti olemasolu ja sisu
+echo '<pre>';
+print_r($menu);
+print_r($item);
+echo '</pre>';
+?>
