6970b5fb93e763f29195567f75f3478c81ea1762
KustixD <karl-gustav.kraak@khk.ee>
2017-03-29 14:00:20 +0300

    Creating http object and testing it

--- conf.php
+++ conf.php
@@ -12,4 +12,12 @@ define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi nime konstant
 //võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
+
+// loome vajalikud objektid projekti tööks
+$http = new http();
+$http->init();
+// Testime http objekti tööd
+echo '<pre>';
+print_r($http);
+echo '</pre>';
 ?>
