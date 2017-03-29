b65f6840a840dd4ac14a4e0522128afbdbec533b
KustixD <karl-gustav.kraak@khk.ee>
2017-03-29 15:33:49 +0300

    Added links to menu2.0

--- menu.php
+++ menu.php
@@ -19,6 +19,9 @@ $item->set('link', $link);
 $menu->set('items', $item->parse());
 //
 $item->set('name', 'teine');
+$link = $http->getLink(array('act'=>'second'));
+$item->set('link', $link);
+
 $menu->add('items', $item->parse());
 // kontrollime objekti olemasolu ja sisu
 // kui soovime pidevat asendamist, siis set funktsioon add asemel
