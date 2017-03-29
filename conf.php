ef39e5617c7b84fc82fa94a007146ef6f88513fb
KustixD <karl-gustav.kraak@khk.ee>
2017-03-29 13:55:02 +0300

    Took http class to use

--- conf.php
+++ conf.php
@@ -11,4 +11,5 @@ define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi nime konstant
 
 //võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
+require_once CLASSES_DIR.'http.php';
 ?>
