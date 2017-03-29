d01a0d5906aa38e7a5640d8e7cc3c07f0559a46c
KustixD <karl-gustav.kraak@khk.ee>
2017-03-29 14:12:54 +0300

    Added required constants to use

--- classes/http.php
+++ classes/http.php
@@ -12,10 +12,21 @@ class http
     var $vars = []; // http päringute andmed
     var $server = []; // serveri masina andmed
     // klassi meetodid
+
     // paneme algandmed paika - initsialiseerime neid
     function init(){
         $this->vars = array_merge($_GET, $_POST, $_FILES);
         $this->server = $_SERVER;
-    }
+    } // init
+
+    // defineerime vajalikud konstandid
+    function initCont(){
+        $consts = array('REMOTE_ADDR', 'HTTP_HOST', 'PHP_SELF', 'SCRIPT_NAME');
+        foreach ($consts as $const){
+            if(!defined($const) and isset($this->server[$const])){
+                define($const, $this->server[$const]);
+            }
+        }
+    }//initCont
 
 } // klassi lõpp
