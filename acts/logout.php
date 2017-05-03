a117cc658464379c61148fee8b91184982c641c8
KustixD <karlgustav14@gmail.com>
2017-05-03 11:09:20 +0300

    Added logout

---
+++ acts/logout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KustixD
+ * Date: 03.05.2017
+ * Time: 11:08
+ */
+$sess->delSession(); // kustutame olemas olev sessioon
+$http->redirect();// suuname pealehele
+?>
