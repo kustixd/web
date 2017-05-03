bb05a85e0e2884550db0c16c254c3aaa2e4e399f
KustixD <karlgustav14@gmail.com>
2017-05-03 11:02:01 +0300

    Improved error showing

--- classes/session.php
+++ classes/session.php
@@ -131,7 +131,18 @@ class session { //klassi algus
     function set($name, $val){
         $this->vars[$name]=$val;
     } //set
+
     // sessiooni andmete võtmine
-    function get
+    function get($name){
+        if(isset($this->vars[$name])){
+            return $this->vars[$name];
+        }
+        return false;
+    }// get
+    function del($name){
+        if(isset($this->vars[$name])){
+            unset($this->vars[$name]);
+        }
+    }// del
 }// klassi lõpp
 ?>
